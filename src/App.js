1a46e977cb986b0f4c1e0390bc7ede4e7719fe86
joaoalado <joaobarbanti7@gmail.com>
2022-12-10 01:51:42 -0300

    arrumando rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-
 import React from "react";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import Books from "./components/Book/Books";
-import BookDetails from "./components/Book/BookDetails";
+import About from "./components/About";
+import BookDetail from "./components/Book/BookDetail";
 function App() {
   return (
-  
-  
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
       <main>
         <Routes>
-<Route path="/" element={<Home />} exact />  
-          <Route path="/" element={<AddBook />} exact />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/add" element={<AddBook />} exact />
           <Route path="/books" element={<Books />} exact />
-          <Route path="/books/:id" element={<BookDetails />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/books/:id" element={<BookDetail />} exact />
         </Routes>
       </main>
-
+    </React.Fragment>
   );
 }
 
